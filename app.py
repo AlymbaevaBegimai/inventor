18cdf830f68cd17e3ac2b25f59f0b119ee371b60
Begimai <sally.vatanabe.2002@gmail.com>
2021-05-28 20:45:31 +0600

    added adding

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,3 +6,14 @@ def homepage():
     f = open('goods.txt', 'r', encoding='utf8')
     txt = f.readlines()
     return render_template('index.html', goods=txt) 
+
+@app.route('/add/', methods=['POST'])
+def add():
+    good = request.form['good']
+    f = open('goods.txt', 'a+', encoding='utf8')
+    f.write(good + '\n')
+    f.close()
+    return """
+        <h1>Инвентарь пополнен</h1>
+        <a href='/'>Домой</a>
+    """
